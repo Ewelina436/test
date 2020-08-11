930a092a48ed770a02c672ab838f9d3220e002a1
Ewelina <e.goraca@musq.io>
2020-08-11 10:57:58 +0200

    fix to the second branch

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
 
-echo "testujemy gita jeszcze raz sprawdzam tagi lekki kolejność gałąź1_comeback";
+echo "testujemy gita jeszcze raz sprawdzam tagi lekki kolejność gałąź1_comeback POPRAWIONO";
 
 echo "prace na gałęźi_1";
