dd5c76871bfe7e8520a0c9ac1d979f75cab6916e
Ewelina <e.goraca@musq.io>
2020-08-12 13:05:25 +0200

    track-branch

--- test.php
+++ test.php
@@ -2,16 +2,7 @@
 
 
 
-echo "testujemy gita jeszcze raz sprawdzam tagi lekki kolejność gałąź1_comeback POPRAWIONO";
+echo "track-branch";
 
-echo "prace na gałęźi_1";
 
-////One more time:
-echo "first branch update";
-
-echo "wyslij i zaktualizuj";
-
-echo "nowa linijka";
-
-echo "uczymy się śledzić nową gałąź teraz";
 
