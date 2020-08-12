b76dee5f56e45e14414cb81408321bc9d2edc34c
Ewelina <e.goraca@musq.io>
2020-08-12 12:40:36 +0200

    master

--- test.php
+++ test.php
@@ -2,17 +2,4 @@
 
 
 
-echo "testujemy gita jeszcze raz sprawdzam tagi lekki kolejność gałąź1_comeback POPRAWIONO";
-
-echo "prace na gałęźi_1";
-
-////One more time:
-echo "first branch update";
-
-echo "wyslij i zaktualizuj";
-
-echo "nowa linijka";
-
-echo "uczymy się śledzić nową gałąź teraz";
-echo "uczymy się śledzić nową gałąź ZMIANKA ZMIANY test";
-echo "NOWE";
+echo "master";
